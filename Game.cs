a8493162461ffc6ae2d0bd9f6d9bd14a8318cd20
František Šilhán <mathiasgj1.1234@gmail.com>
2023-01-24 21:16:04 +0100

    tostring očištěn

--- Game.cs
+++ Game.cs
@@ -39,46 +39,18 @@ namespace TowerOfHanoi
 
         public override string ToString()
         {
-            /*string state = "";
-            foreach (Stack<int> i in Towers)
-            {
-                state += string.Join("", i);
-                state += " | ";
-            }
-            return state;*/
             string state = "";
             int biggest = 0;
-
-            foreach (Stack<int> stack in Towers) {
-                if (stack.Count > biggest) {
-                    biggest = stack.Count;
-                }
-            }
-
+            foreach (Stack<int> stack in Towers) if (stack.Count > biggest) biggest = stack.Count;
             for (int i = biggest-1; i >= 0; i--) {
                 for (int j = 0; j < Towers.Length; j++) {
-                    if (j == 0)
-                    {
-                        state += "|";
-                    }
-
-                    if (i >= Towers[j].Count) {
-                        /*Console.Write(" ");*/
-                        state += " ";
-                    } else {
-                        /*Console.WriteLine(Towers[j].ElementAt(i) + " " + i + " " + j);*/
-                        /*Console.Write(Towers[j].ElementAt(i));*/
-                        state += Towers[j].ElementAt(Towers[j].Count-1-i);
-                    }
-
-                    /*Console.Write("|");*/
+                    if (j == 0) state += "|";
+                    if (i >= Towers[j].Count) state += " ";
+                    else state += Towers[j].ElementAt(Towers[j].Count-1-i);
                     state += "|";
                 }
-
-                /*Console.Write('\n');*/
                 state += "\n";
             }
-
             return state;
         }
     }
